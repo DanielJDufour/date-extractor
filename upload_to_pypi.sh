ee52ec96877a3a3c24f8e40458a1105b73eb1638
x <x>
2015-12-04 03:44:37 +0000

    commit

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 version=$(cat setup.py  | grep version | grep -oP '(\d.\d)')
-echo 'version is' $version
+echo 'old version is' $version
 left=$(echo $version | grep -oP '\d(?=.)')
-echo 'left is' $left
 right=$(echo $version | grep -oP '(?<=.)\d')
-echo 'right is' $right
 
 if (( right == 9 )); then
-    echo "right is 9"
     right=0
     left=$((left+1))
-    echo "left is" $left
 else
-    echo "right is not 9, so just add 1 to right"
     right=$((right+1))
 fi
 version="$left.$right"
-echo "version is" $version
+echo "new version is" $version
 
 # replace version in setup.py
 sed -i "s/.*version.*/  version = '$version',/" setup.py
+
+git add --all :/
+git commit
 
 git tag $version -m "Adds a tag so that we can put this on PyPI."
 
