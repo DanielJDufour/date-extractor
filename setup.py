48e17934ae201d6e5d01ed111916c58142a392c2
DanielJDufour <daniel.j.dufour@gmail.com>
2019-10-06 13:07:34 -0400

    added long_description

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from distutils.core import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
   name = 'date-extractor',
   packages = ['date_extractor'],
   package_dir = {'date_extractor': 'date_extractor'},
   package_data = {'date_extractor': ['arabic.py', 'enumerations.py', '__init__.py', 'data/months_verbose/arabic.txt', 'data/months_verbose/french.txt', 'data/months_verbose/sorani.txt', 'data/months_verbose/turkish.txt', 'tests/__init__.py', 'tests/test.py']},
-  version = '4.0.1',
+  version = '4.0.2',
   description = 'Extract dates from text',
+  long_description = long_description,
+  long_description_content_type='text/markdown',
   author = 'Daniel J. Dufour',
   author_email = 'daniel.j.dufour@gmail.com',
   url = 'https://github.com/DanielJDufour/date-extractor',
