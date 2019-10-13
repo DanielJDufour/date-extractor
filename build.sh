80170dce5e54004004cb84258deae0fb8ddf2bf5
DanielJDufour <daniel.j.dufour@gmail.com>
2019-10-12 20:25:09 -0400

    updated build.sh to wipe away previous builds

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+# wipe away any previous builds
+rm -fr dist
+
 # make sure libraries used for publishing are up to date
 python3 -m pip install --user --upgrade setuptools wheel twine
 pip install --upgrade twine
